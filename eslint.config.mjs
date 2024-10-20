40ac3ad08e16cadc83b0e5965d3a62315250104a
rinwi-smith <rinwismith@gmail.com>
2024-10-20 10:22:40 +0100

    fixing linting issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,16 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
-
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    files: ["*.js"],
+    languageOptions: {
+      globals: {
+        require: "readonly",   // Enable 'require' as a global variable
+        describe: "readonly",  // Enable 'describe' from Mocha
+        it: "readonly",        // Enable 'it' from Mocha
+      }
+    },
+    rules: {
+      "no-console": "off",     // Example rule to turn off 'console' warnings
+      "no-undef": "off"        // Turn off undefined variable warnings for test environments
+    }
+  }
 ];
